1d428dff5697cb6a17493ad12c3957816e83d6db
SolomonAnn <thss15_anyz@163.com>
2018-12-25 11:34:48 +0800

    Add RandomizedLogisticRegression.

--- src/regression/logistic_regression.py
+++ src/regression/logistic_regression.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression as LR
+from sklearn.linear_model import RandomizedLogisticRegression as RLR
 import time
 import csv
 
@@ -25,8 +26,12 @@ def read_csv(filename, dataset):
     with open(filename, "r") as csv_file:
         csv_reader = csv.reader(csv_file)
         header = next(csv_reader)
+        header.pop(1)
+        header.pop(0)
         for row in csv_reader:
             if row[15][0:3] == "250":
+                row.pop(1)
+                row.pop(0)
                 dataset.append(row)
 
         print("**********")
@@ -36,17 +41,18 @@ def read_csv(filename, dataset):
     return header
 
 # write the 'regression.csv'
-def write_csv(header, records):
+def write_csv(header, dataset):
     with open("regression.csv", "w") as csv_file:
         csv_writer = csv.writer(csv_file, lineterminator = "\n")
         csv_writer.writerow(header)
-        csv_writer.writerows(records)
+        csv_writer.writerows(dataset)
 
+# logistic regression
 def logistic_regression():
     filename = 'regression.csv'
     dataset = pd.read_csv(filename)
-    x = dataset.iloc[:,2:44].values
-    y = dataset.iloc[:,44].values
+    x = dataset.iloc[:,0:42].values
+    y = dataset.iloc[:,42].values
 
     for i in set(range(0, 6)) | set(range(15, 42)):
         value = 0
@@ -56,17 +62,27 @@ def logistic_regression():
                 value_dict[item[i]] = value
                 value += 1
             item[i] = value_dict[item[i]]
-            
-    for item in x:
-        for i in range(len(item)):
-            if item[i] == "unknown":
-                print("1")
     
     for i in range(len(y)):
         if y[i] == "NO":
             y[i] = 1
         else:
             y[i] = 0
+
+    rlr = RLR()
+    rlr.fit(x, y.astype("int"))
+    rlr.get_support(indices=True)
+    print("Attributes: {0}".format(",".join(dataset.columns[rlr.get_support(indices=True)])))
+    x = dataset[dataset.columns[rlr.get_support(indices=True)]].values
+
+    for i in {0, 1, 10, 11}:
+        value = 0
+        value_dict = {}
+        for item in x:
+            if item[i] not in value_dict.keys():
+                value_dict[item[i]] = value
+                value += 1
+            item[i] = value_dict[item[i]]
 
     lr = LR()
     lr.fit(x, y.astype("int"))
